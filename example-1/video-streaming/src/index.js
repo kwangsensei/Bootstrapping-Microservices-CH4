630595f5586fecdc3ca02727302d3e80e52a4db4
Ashley Davis <ashley@codecapers.com.au>
2022-08-03 07:42:21 +1000

    Updated code to use async/await.

--- example-1/video-streaming/src/index.js
+++ example-1/video-streaming/src/index.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const fs = require("fs");
-const path = require("path");
-
-const app = express();
 
 //
 // Throws an error if the PORT environment variable is missing.
@@ -12,34 +9,36 @@ if (!process.env.PORT) {
 }
 
 //
-// Extracts the PORT environment variable.
+// Extracts environment variables to globals for convenience.
 //
 const PORT = process.env.PORT;
 
 //
-// Registers a HTTP GET route for video streaming.
+// Application entry point.
 //
-app.get("/video", (req, res) => {
+async function main() {
 
-    const videoPath = path.join("./videos", "SampleVideo_1280x720_1mb.mp4");
-    fs.stat(videoPath, (err, stats) => {
-        if (err) {
-            console.error("An error occurred ");
-            res.sendStatus(500);
-            return;
-        }
+    const app = express();
 
+    app.get("/video", async (req, res) => { // Route for streaming video.
+        
+        const videoPath = "./videos/SampleVideo_1280x720_1mb.mp4";
+        const stats = await fs.promises.stat(videoPath);
+    
         res.writeHead(200, {
             "Content-Length": stats.size,
             "Content-Type": "video/mp4",
         });
         fs.createReadStream(videoPath).pipe(res);
     });
-});
 
-//
-// Starts the HTTP server.
-//
-app.listen(PORT, () => {
-    console.log(`Microservice online.`);
-});
+    app.listen(PORT, () => {
+        console.log("Microservice online.");
+    });
+}
+
+main()
+    .catch(err => {
+        console.error("Microservice failed to start.");
+        console.error(err && err.stack || err);
+    });
